4ded2140b9e6b341637e2ff3c8308d2d4becf663
justynke <j.dabrowska@gmx.de>
2020-12-23 10:25:59 +0100

    add guessing whole word functionality

--- hangman.py
+++ hangman.py
@@ -1,12 +1,27 @@
 from random import choice
 
 
+# GAME INTRO
 
-print("H A N G M A N\nThe game will be available soon.")
+print("H A N G M A N\n")
 words = ['python', 'java', 'kotlin', 'javascript']
 chosen_word = choice(words)
-word = input("Guess the word:" + chosen_word[0:3] + (len(chosen_word)-3)*"-")
-if word == chosen_word:
-    print("You survived!")
-else:
-    print("You lost!")
+temp_word = list(len(chosen_word)*"-")
+
+
+tries = 0
+
+while tries < 8:
+
+    print("".join(temp_word))
+    char = input("Input a letter:")
+    if char in chosen_word:
+        print()
+        for i in range (len(chosen_word)):
+            if chosen_word[i] == char:
+                temp_word[i] = char
+    else:
+        print("That letter doesn't appear in the word\n")
+    tries += 1
+
+print("Thanks for playing!\nWe'll see how well you did in the next stage")
