5900948ff1702a72dd863a3800e8adff39c156e6
justynke <j.dabrowska@gmx.de>
2020-12-23 11:43:55 +0100

    add menu and improved PEP8 requirements

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 def player_input():
     while True:
         znak = input("Input a letter:")
@@ -21,16 +22,23 @@ def player_input():
                 print("".join(temp_word))
 
 
+def menu():
+    while True:
+        menu_choice = input()
+        print('Type "play" to play the game, "exit" to quit:')
+        if menu_choice == "exit":
+            exit()
+        elif menu_choice == "play":
+            break
 
 
 # GAME INTRO
 
 print("H A N G M A N")
+menu()
 words = ['python', 'java', 'kotlin', 'javascript']
 chosen_word = choice(words)
 temp_word = list(len(chosen_word)*"-")
-
-
 
 # GAME
 guessed_letters = set()
@@ -41,7 +49,7 @@ while lives > 0:
     char = player_input()
     guessed_letters.add(char)
     if char in chosen_word:
-        for i in range (len(chosen_word)):
+        for i in range(len(chosen_word)):
             if chosen_word[i] == char:
                 temp_word[i] = char
     else:
